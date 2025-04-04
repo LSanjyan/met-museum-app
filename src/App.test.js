02c79759e358d28332506f4b348cf7d4d6b5ed7b
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2025-04-04 14:12:16 -0400

    Some Unit testes added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Artwork from "./pages/Artwork";
 
-test('renders learn react link', () => {
+jest.mock("./pages/Home", () => () => <div>Home Page</div>);
+jest.mock("./pages/Artwork", () => () => <div>Artwork - ID</div>);
+jest.mock("./pages/Artwork", () => {
+  return function MockArtwork() {
+    return <div>Loading artwork...</div>;
+  };
+});
+
+test("renders Home page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("Home Page")).toBeInTheDocument();
+});
+test("renders Artwork page when navigating to /artwork/:id", () => {
+  render(
+    <MemoryRouter initialEntries={["/artwork/123"]}>
+      <Routes>
+        <Route path="artwork/:id" element={<div>Artwork - ID</div>} />
+      </Routes>
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText("Artwork - ID")).toBeInTheDocument();
+});
+test("displays loading state initially", () => {
+  render(
+    <MemoryRouter initialEntries={["/artwork/789"]}>
+      <Artwork />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText("Loading artwork...")).toBeInTheDocument();
 });
