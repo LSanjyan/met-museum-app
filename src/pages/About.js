89b385d1c3582e0c6028efafaab606acaa77904b
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2025-04-04 18:22:27 -0400

    Added changes in About and Contact Components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./About.css";
 
 function About() {
+  const navigate = useNavigate();
   return (
     <div className="about-container">
-      <h2>About Us</h2>
-      <p>
-        Welcome to the Metropolitan Museum Art Gallery! We are dedicated to
-        showcasing the finest art collections from around the world. Our mission
-        is to inspire and educate visitors through the beauty and diversity of
-        art.
-      </p>
-      <p>
-        Our gallery features a wide range of artworks, including paintings,
-        sculptures, and decorative arts from various cultures and time periods.
-        We strive to provide an enriching experience for all art enthusiasts,
-        scholars, and casual visitors alike.
-      </p>
+      <h2 className="about-title">About Us</h2>
+      <div className="about-content">
+        <div className="about-text">
+          <p>Welcome to the Metropolitan Museum Art Gallery!</p>
+          <p>
+            Our gallery features a wide range of artworks, including paintings,
+            sculptures, and decorative arts from various cultures and time
+            periods.
+          </p>
+          <p>
+            We strive to provide an enriching experience for all art
+            enthusiasts, scholars, and casual visitors alike.
+          </p>
+          <p>
+            You can see Museum Map and explore the different sections of the
+            museum
+          </p>
+        </div>
+        <div className="about-image">
+          <img src="met_map.png" alt="Met Museum Map" />
+        </div>
+      </div>
+      <button className="home-btn" onClick={() => navigate("/")}>
+        Home
+      </button>
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Contact() {
-  return <div>Contact</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="about-container">
+      <h2 className="about-title">Contact</h2>
+      <div className="about-content">
+        <div className="about-text">
+          <p>Contact Us Via Email: mail@mail.com </p>
+          <p>Phone: 123-456-7890</p>
+          <p>Address: 100 Avenue, Arctica </p>
+        </div>
+      </div>
+      <button className="home-btn" onClick={() => navigate("/")}>
+        Home
+      </button>
+    </div>
+  );
 }
 
 export default Contact;
